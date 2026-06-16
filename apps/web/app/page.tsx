2007c39ec1d1635e3ead9580179a42241c49c8cc
kamal-gogroup <kamal.pandey@gogroup.co>
2026-06-16 17:33:39 +0530

    initial commit

---
+++ apps/web/app/page.tsx
@@ -0,0 +1,24 @@
+"use client"
+
+import { useMutation, useQuery } from "convex/react"
+import { api } from "@workspace/backend/_generated/api"
+import { Button } from "@workspace/ui/components/button"
+
+export default function Page() {
+  const users = useQuery(api.users.getMany)
+  const addUsers = useMutation(api.users.add)
+  return (
+    <div className="flex min-h-svh p-6">
+      <div className="flex max-w-md min-w-0 flex-col gap-4 text-sm leading-loose">
+        <div>
+          <p>Web page</p>
+          <Button onClick={() => addUsers()}>Add user</Button>
+          <p>Users in database: {JSON.stringify(users)}</p>
+        </div>
+        <div className="font-mono text-xs text-muted-foreground">
+          (Press <kbd>d</kbd> to toggle dark mode)
+        </div>
+      </div>
+    </div>
+  )
+}
