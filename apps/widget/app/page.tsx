1ea20b86be9482b2ee4ff8b3466d0112dcf3aae8
kamal-gogroup <kamal.pandey@gogroup.co>
2026-06-18 00:27:40 +0530

    widget screen setup

--- apps/widget/app/page.tsx
+++ apps/widget/app/page.tsx
@@ -1,24 +1,16 @@
 "use client"
+import { use } from "react"
+import { WidgetView } from "@/modules/widget/ui/views/widget-view"
 
-import { useMutation, useQuery } from "convex/react"
-import { api } from "@workspace/backend/_generated/api"
-import { Button } from "@workspace/ui/components/button"
-
-export default function Page() {
-  const users = useQuery(api.users.getMany)
-  const addUsers = useMutation(api.users.add)
-  return (
-    <div className="flex min-h-svh p-6">
-      <div className="flex max-w-md min-w-0 flex-col gap-4 text-sm leading-loose">
-        <div>
-          <p>Widget page</p>
-          <Button onClick={() => addUsers()}>Add user</Button>
-          <p>Users in database: {JSON.stringify(users)}</p>
-        </div>
-        <div className="font-mono text-xs text-muted-foreground">
-          (Press <kbd>d</kbd> to toggle dark mode)
-        </div>
-      </div>
-    </div>
-  )
+interface Props {
+  searchParams: Promise<{
+    organizationId: string
+  }>
 }
+
+const Page = ({ searchParams }: Props) => {
+  const { organizationId } = use(searchParams)
+  return <WidgetView organizationId={organizationId} />
+}
+
+export default Page
