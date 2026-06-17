6f09cd1c1de395062b62d561fcb4357c5231c28a
kamal-gogroup <kamal.pandey@gogroup.co>
2026-06-18 02:46:52 +0530

    router added

--- apps/widget/components/providers.tsx
+++ apps/widget/components/providers.tsx
@@ -3,13 +3,16 @@
 import * as React from "react"
 import { ConvexProvider, ConvexReactClient } from "convex/react"
 import { TooltipProvider } from "@workspace/ui/components/tooltip"
+import { Provider } from "jotai"
 
 const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL || "")
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <ConvexProvider client={convex}>
-      <TooltipProvider>{children}</TooltipProvider>
+      <TooltipProvider>
+        <Provider>{children}</Provider>
+      </TooltipProvider>
     </ConvexProvider>
   )
 }
--- apps/widget/modules/widget/ui/views/widget-view.tsx
+++ apps/widget/modules/widget/ui/views/widget-view.tsx
@@ -1,18 +1,31 @@
 "use client"
 
-import { WidgetAuthScreen } from "../screens/widget-auth-screen"
-
-// import { WidgetFooter } from "../components/widget-footer"
+import { useAtomValue } from "jotai"
+import { WidgetAuthScreen } from "@/modules/widget/ui/screens/widget-auth-screen"
+import { screenAtom } from "@/modules/widget/atoms/widget-atoms"
 
 interface Props {
   organizationId: string
 }
 
 export const WidgetView = ({ organizationId }: Props) => {
+  const screen = useAtomValue(screenAtom)
+
+  const screenComponents = {
+    error: <p>TODO: Error</p>,
+    loading: <p>TODO: Loading</p>,
+    auth: <WidgetAuthScreen />,
+    voice: <p>TODO: Voice</p>,
+    inbox: <p>TODO: Inbox</p>,
+    selection: <p>TODO: Selection</p>,
+    chat: <p>TODO: Chat</p>,
+    contact: <p>TODO: Contact</p>,
+  }
+
   return (
-    <main className="flex h-full min-h-screen w-full flex-col overflow-hidden rounded-xl border bg-muted">
-      <WidgetAuthScreen />
-      {/* <WidgetFooter /> */}
+    // TODO: Confirm whether or not "min-h-screen" and "min-w-screen" is needed
+    <main className="flex h-full min-h-screen w-full min-w-screen flex-col overflow-hidden rounded-xl border bg-muted">
+      {screenComponents[screen]}
     </main>
   )
 }
